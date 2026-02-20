f22e5aefca63447c05022a9f4fd1736a7bde35aa
Sakhwat Hossain Saikot <saikot10102008@gmail.com>
2026-02-20 17:47:26 +0600

    simple CLI based calculator program

--- detailed_notes/14_console_calculator_program.cpp
+++ detailed_notes/14_console_calculator_program.cpp
@@ -2,8 +2,43 @@
 
 int main(){
 
+    double a,b,ans;
+    char op;
 
+    std::cout<<"Chose one of the operations (+,-,/,*): ";
+    std::cin>>op;
+
+    std::cout<<"Enter 1st number: ";
+    std::cin>>a;
+
+    std::cout<<"Enter 2nd number: ";
+    std::cin>>b;
+
+    switch (op)
+    {
+    case '+':
+        ans = a+b;
+        break;
     
+    case '-':
+        ans = a-b;
+        break;
+
+    case '*':
+        ans = a*b;
+        break;
+
+    case '/':
+        ans = a/b;
+        break;
+    
+    default:
+        std::cout<<"Operator not valid";
+        break;
+    }
+
+    std::cout<<"Your answer is: "<<ans;
+
     return 0;
 }
 
